1559c55cfa042d291a8f219dd93ca0a4f4c770e6
yonchu <yuyuchu3333@gmail.com>
2012-12-20 12:02:12 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='python-daemon',
+    version='0.0.1',
+    url='',
+    license='MIT',
+    author='Yonchu',
+    author_email='yuyuchu3333@gmail.com',
+    description='forked from https://github.com/serverdensity/python-daemon'
+)
